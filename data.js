abdd6e84a0c289a23c91cbea6715f459db2adf65
Konstantin Biriukov <kostya-leon@mail.ru>
2021-11-30 19:20:20 -0600

    init commit

--- data.js
+++ data.js
@@ -1,62 +1,62 @@
-[
+export default [
     {
         "id": 1,
         "name": "Петя",
-        "friends": [10,2,6]
+        "friends": [10, 2, 6]
     },
     {
         "id": 2,
         "name": "Вася",
-        "friends": [5,1,3]
+        "friends": [5, 1, 3]
     },
     {
         "id": 3,
         "name": "Оля",
-        "friends": [9,4,3]
+        "friends": [9, 4, 3]
     },
     {
         "id": 4,
         "name": "Максим",
-        "friends": [11,12,2]
+        "friends": [11, 12, 2]
     },
     {
         "id": 5,
         "name": "Елена",
-        "friends": [7,8,4]
+        "friends": [7, 8, 4]
     },
     {
         "id": 6,
         "name": "Иван",
-        "friends": [6,1,12]
+        "friends": [6, 1, 12]
     },
     {
         "id": 7,
         "name": "Никита",
-        "friends": [1,8,5]
+        "friends": [1, 8, 5]
     },
     {
         "id": 8,
         "name": "Марат",
-        "friends": [11,12,10]
+        "friends": [11, 12, 10]
     },
     {
         "id": 9,
         "name": "Анатолий",
-        "friends": [1,2,3]
+        "friends": [1, 2, 3]
     },
     {
         "id": 10,
         "name": "Наташа",
-        "friends": [8,4,2]
+        "friends": [8, 4, 2]
     },
     {
         "id": 11,
         "name": "Марина",
-        "friends": [1,5,8]
+        "friends": [1, 5, 8]
     },
     {
         "id": 12,
         "name": "Кирилл",
-        "friends": [5,2,12]
+        "friends": [5, 2, 12]
     }
 ]
